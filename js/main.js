92d03576dca1759b50fb907c497d4c3cd8d1006d
ltqqui <107937310+ltqqui@users.noreply.github.com>
2022-09-09 22:41:56 +0700

    khong

---
+++ js/main.js
@@ -0,0 +1,143 @@
+let dataGlasses = [
+  {
+    id: "G1",
+    src: "./img/g1.jpg",
+    virtualImg: "./img/v1.png",
+    brand: "Armani Exchange",
+    name: "Bamboo wood",
+    color: "Brown",
+    price: 150,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? ",
+  },
+  {
+    id: "G2",
+    src: "./img/g2.jpg",
+    virtualImg: "./img/v2.png",
+    brand: "Arnette",
+    name: "American flag",
+    color: "American flag",
+    price: 150,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G3",
+    src: "./img/g3.jpg",
+    virtualImg: "./img/v3.png",
+    brand: "Burberry",
+    name: "Belt of Hippolyte",
+    color: "Blue",
+    price: 800,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G4",
+    src: "./img/g4.jpg",
+    virtualImg: "./img/v4.png",
+    brand: "Coarch",
+    name: "Cretan Bull",
+    color: "Red",
+    price: 600,
+    description: "In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G5",
+    src: "./img/g5.jpg",
+    virtualImg: "./img/v5.png",
+    brand: "D&G",
+    name: "JOYRIDE",
+    color: "Gold",
+    price: 180,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?",
+  },
+  {
+    id: "G6",
+    src: "./img/g6.jpg",
+    virtualImg: "./img/v6.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Blue, White",
+    price: 320,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G7",
+    src: "./img/g7.jpg",
+    virtualImg: "./img/v7.png",
+    brand: "Ralph",
+    name: "TORTOISE",
+    color: "Black, Yellow",
+    price: 500,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam.",
+  },
+  {
+    id: "G8",
+    src: "./img/g8.jpg",
+    virtualImg: "./img/v8.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Red, Black",
+    price: 555,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim.",
+  },
+  {
+    id: "G9",
+    src: "./img/g9.jpg",
+    virtualImg: "./img/v9.png",
+    brand: "Coarch",
+    name: "MIDNIGHT VIXEN REMIX",
+    color: "Blue, Black",
+    price: 333,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet.",
+  },
+];
+
+function renderProductSrc() {
+  let html = "";
+  for (let prod of dataGlasses) {
+    html += `
+    <a class="col-4 py-3" href="?paramProd=${prod.id}"><img class="w-100" src=${prod.src} alt=${prod.name} /></a>
+            `;
+  }
+  document.querySelector("#vglassesList").innerHTML = html;
+}
+renderProductSrc();
+
+
+let urlParams=new URLSearchParams(window.location.search);
+let myParam=urlParams.get('paramProd');
+
+function getGlasses(arrGlass , k){
+  let htmlAvatar;
+  let htmlInfo;
+  for(let i=0;i<arrGlass.length;i++){
+    if(arrGlass[i].id==k){
+      // console.log(arrGlass[i]);
+      htmlAvatar=`
+       <div class="item">
+       <img src="${arrGlass[i].virtualImg}" alt="">
+   </div> 
+  `;
+  htmlInfo=`
+<div class="row">
+  <div class="item">
+      <h3 style="display:inline;">${arrGlass[i].name}</h3><h3 style=" display:inline; size:1.25rem">-${arrGlass[i].brand}</h3><h3>(${arrGlass[i].color})</h3>
+      <a href="#">${arrGlass[i].price}</a>
+      <span>Stocking</span>
+      <p>${arrGlass[i].description}</p>
+  </div>
+</div>
+  `
+    }
+  }
+ document.querySelector('#avatar').innerHTML=htmlAvatar;
+ document.querySelector('#glassesInfo').innerHTML=htmlInfo;
+ removeGlasses(true);
+}
+getGlasses(dataGlasses, myParam);
+
---
+++ models/Glasses.js
@@ -0,0 +1,10 @@
+function Glasses(){
+    this.id='';
+    this.src='';
+    this.virtualImg='';
+    this.brand='';
+    this.name='';
+    this.color='';
+    this.price='';
+    this.description='';
+}
